8b5db0f8b57654a1f467b1c3c03441526271adb9
AnandJNairGit <anandjnairit@gmail.com>
2022-11-05 23:07:54 +0530

    create server

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
-console.log("node intro");
-console.log(global);
+const http = require('http');
+const app = require('./REST/app');
+const port = 3000;
 
-const os = require("os");
-console.log(os.type());
-console.log(os.version());
-console.log(os.homedir());
+const server = http.createServer(app);
+server.listen(port);
+console.log("helo");
 
-console.log(__dirname);
-console.log(__filename);
 
-const path = require("path");
-const { add, sub, mul, div } = require("./math");
-console.log(path.dirname(__filename));
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
+// console.log("node intro");
+// console.log(global);
 
-console.log(path.parse(__filename));
-console.log(path.parse(__filename).name);
+// const os = require("os");
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
 
-console.log(add(1,2));
-console.log(sub(1,2));
-console.log(mul(1,2));
-console.log(div(1,2));
+// console.log(__dirname);
+// console.log(__filename);
+
+// const path = require("path");
+// const { add, sub, mul, div } = require("./math");
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+
+// console.log(path.parse(__filename));
+// console.log(path.parse(__filename).name);
+
+// console.log(add(1,2));
+// console.log(sub(1,2));
+// console.log(mul(1,2));
+// console.log(div(1,2));
 
